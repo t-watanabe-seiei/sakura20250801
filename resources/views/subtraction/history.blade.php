255d2f8b7ba0710aadf39832ef572831236c88c4
takashi-watanabe <139939468+t-watanabe-seiei@users.noreply.github.com>
2025-07-31 02:25:08 +0000

    7th

--- resources/views/subtraction/history.blade.php
+++ resources/views/subtraction/history.blade.php
@@ -39,7 +39,7 @@
                 @endforeach
             </tbody>
         </table>
-        <a href="/" class="next-btn">もんだいに戻る</a>
+        <a href="{{ config('app.mix_url') }}" class="next-btn">もんだいに戻る</a>
     </div>
 </body>
 </html>
